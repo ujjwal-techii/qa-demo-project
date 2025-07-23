18926b558ed124411332c04124f0d5dfec4efa66
adam <Haseeb.mehraj@sceninjaz.com>
2025-07-23 15:51:59 +0530

    update login file

--- test_cases/login_test.py
+++ test_cases/login_test.py
@@ -11,10 +11,14 @@ response = requests.get(url)
 # Checking response status
 if response.status_code == 200:
     pretty_json =  json.dumps(response.json(), indent=4)
+    pretty_json = json.dumps(response.json(), indent=4)
+    pretty_json = json.dumps(response.json(), indent=4)
+    pretty_json = json.dumps(response.json(), indent=4)
+    pretty_json = json.dumps(response.json(), indent=4)
     print(pretty_json)  # Parse JSON response
 else:
     print(f"Failed! Status Code: {response.status_code}")
-
+pretty_json =  json.dumps(response.json(), indent=4)
 print ("Testing haseeb")
 
 print("Testing haseeb")
