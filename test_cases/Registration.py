4f2473f6e575aa7140cccd1952517ee5200d0a65
ujjwal <ujjwal.kumar@secninjaz.com>
2025-07-31 10:34:39 +0530

    one more function removed

--- test_cases/Registration.py
+++ test_cases/Registration.py
@@ -3,12 +3,7 @@ class User:
         self.name = name
         self.in_call = False
 
-    def call(self, other_user):
-        if not other_user.in_call:
-            print(f"{self.name} is calling {other_user.name}...")
-            other_user.receive_call(self)
-        else:
-            print(f"{other_user.name} is already on another call.")
+
 
 
 
@@ -19,3 +14,7 @@ if __name__ == "__main__":
 
     alice.call(bob)         # Alice calls Bob
     alice.end_call(bob)     # Alice ends the call
+
+
+
+
