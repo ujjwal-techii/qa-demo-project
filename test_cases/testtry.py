e76e4175c6d2ebdf08eedf113126172501a2af11
adam <Haseeb.mehraj@sceninjaz.com>
2025-08-06 11:12:36 +0530

    feat: Updated the new methods

--- test_cases/testtry.py
+++ test_cases/testtry.py
@@ -6,3 +6,13 @@ response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Hello how are u ?"
 )
 print(response.text)
+
+from google import genai
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+)
+print(response.text)
