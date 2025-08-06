143e87d2b8fb75eae49978e91f5e79b62400450f
adam <Haseeb.mehraj@sceninjaz.com>
2025-08-06 11:40:47 +0530

    feat: added more methods

--- test_cases/testtry.py
+++ test_cases/testtry.py
@@ -16,3 +16,18 @@ response = client.models.generate_content(
     model="gemini-2.5-flash", contents="Explain how AI works in a few words"
 )
 print(response.text)
+
+
+from google import genai
+from google.genai import types
+
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Explain how AI works in a few words",
+    config=types.GenerateContentConfig(
+        thinking_config=types.ThinkingConfig(thinking_budget=0) # Disables thinking
+    ),
+)
+print(response.text)
