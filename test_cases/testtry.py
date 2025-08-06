d6c4b8264070110d77290d09e8ab26b4ecc47e85
adam <Haseeb.mehraj@sceninjaz.com>
2025-08-06 10:57:55 +0530

    in the testtry file add dummy method

--- test_cases/testtry.py
+++ test_cases/testtry.py
@@ -1,2 +1,8 @@
-long_username = "a" * 300 + "@example.com"
-print(long_username)
+from google import genai
+
+client = genai.Client(api_key="AIzaSyDid1_lCayYEAHXgzYXWqNofTFUirvGDZY")
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents="Hello how are u ?"
+)
+print(response.text)
